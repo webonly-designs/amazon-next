1fe4a2096936792cee77dd7c44fcf0945517dc51
Akshay Agarwal <agarwal.akshay003@gmail.com>
2022-05-30 21:56:16 -0500

    Home page layout created

---
+++ components/Layout.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import { 
+    MenuIcon,
+    SearchIcon,
+    ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+export default function Layout({title, children}) {
+  return (
+    <>
+        <Head>
+            <title>{title ? title + " - Amazon" :  "Amazon"}</title>
+            <meta name="description" content="Ecommerce Website" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div>
+            <header>
+                <nav>
+                    <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+
+                        <Link href="/">
+                            <div className="my-1 flex items-center flex-grow sm:flex-grow-0">
+                                <Image
+                                    src="https://links.papareact.com/f90" 
+                                    width={150}
+                                    height={40}
+                                    objectFit="contain"
+                                    className="cursor-pointer"
+                                />
+                            </div>
+                        </Link>
+    
+                        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+                            <input className="p-2 full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text"/>
+                            <SearchIcon className="h-12 p-4" />
+                        </div>
+
+                        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+
+                            <div className="link">
+                                <p>Hello Akshay Agarwal</p>
+                                <p className="font-extrabold md:text-sm">Account & Lists</p>
+                            </div>
+
+                            <div className="link">
+                                <p>Returns</p>
+                                <p className="font-extrabold md:text-sm">& Orders</p>
+                            </div>
+
+                            <div className="relative link flex items-center">
+                                <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+                                <ShoppingCartIcon className="h-8"/>
+                                <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+                        <p className="link flex items-center">
+                            <MenuIcon className="h-6 mr-1"/>
+                            All
+                        </p>
+                        <p className="link">Prime Video</p>
+                        <p className="link">Amazon Business</p>
+                        <p className="link">Today's Deals</p>
+                        <p className="link hidden lg:inline-flex">Electronics</p>
+                        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+                        <p className="link hidden lg:inline-flex">Prime</p>
+                        <p className="link hidden lg:inline-flex">Buy Again</p>
+                        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+                        <p className="link hidden lg:inline-flex">Health And Personal Care</p>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer>
+                <div className="bg-amazon_blue py-4">
+                    <Link href="/">
+                        <div className="flex items-center justify-center flex-grow sm:flex-grow-0">
+                            <Image
+                                src="https://links.papareact.com/f90" 
+                                width={150}
+                                height={40}
+                                objectFit="contain"
+                                className="cursor-pointer"
+                            />
+                        </div>
+                    </Link>
+                    <p className="items-center text-center text-sm font-light pt-1 text-white">Copyright Amazon Next 2022.</p>
+                </div>
+            </footer>
+        </div>
+    </>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["links.papareact.com", "fakestoreapi.com"],
+  },
 }
 
 module.exports = nextConfig
